5e1213c1c00633e0e65fc84986d7622c5c429d01
Corentin Recanzone <corentindevpro@hotmail.com>
2025-03-18 11:04:17 +0100

    m

--- src-nuxt/components/forms/LoginForm.vue
+++ src-nuxt/components/forms/LoginForm.vue
@@ -247,7 +247,7 @@ const signIn: () => Promise<void> = async (): Promise<void> => {
  */
 const goToPageHome: () => Promise<void> = async (): Promise<void> => {
   try {
-    await TauriService.adjustWindowToHome(1038, 660)
+    await TauriService.adjustWindowToHome(1042, 660)
   } catch (error) {
     console.error('goToPageHome error:', error)
   }
