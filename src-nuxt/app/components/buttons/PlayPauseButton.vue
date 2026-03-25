00dc446af7918feffc1138902c8964acfbdc350f
Corentin Recanzone <corentindevpro@hotmail.com>
2026-03-25 21:18:35 +0100

    m

--- src-nuxt/app/components/buttons/PlayPauseButton.vue
+++ src-nuxt/app/components/buttons/PlayPauseButton.vue
@@ -3,11 +3,11 @@
     :variant="props.isPlaying ? 'primary' : 'green'"
     v-tooltip.top.auto="props.isPlaying ? 'Pause download' : 'Resume download'"
     @click.stop.prevent="props.isPlaying ? emit('pause') : emit('play')"
-    class="h-10 w-10 min-w-10 px-0 py-0 flex items-center justify-center"
+    class="h-8 w-8 px-0 py-0"
     size="sm"
   >
-    <CrzIcon v-if="props.isPlaying" class="pointer-events-none" name="pause" mode="stroke" :width="20" :height="20" />
-    <CrzIcon v-else class="pointer-events-none" name="play" mode="stroke" :width="20" :height="20" />
+    <CrzIcon v-if="props.isPlaying" class="absolute" name="pause" mode="stroke" :width="20" :height="20" />
+    <CrzIcon v-else class="absolute" name="play" mode="stroke" :width="20" :height="20" />
   </CrzButton>
 </template>
 
